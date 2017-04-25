ed5bec1e1c398579b35b9aaf8221f254f17578c1
tems4real <oreoluwa.onatemowo@gmail.com>
2017-04-25 16:56:10 -0400

    Can perform shallow summation of numbers

--- fp.rkt
+++ fp.rkt
@@ -2,10 +2,20 @@
 ;;; and performs a deep reverse of L
 (DEFINE (reverse-general L)
         (COND
-        ((NULL? L) '()) ;;;If L is NULL return empty list
-        ((LIST? (CAR L)) (append (reverse-general (CDR L))  
-                           (LIST (reverse-general (CAR L)))))
-        (ELSE (append (reverse-general (CDR L))    
-                                 (LIST (CAR L)))))
+             ((NULL? L) '()) ;;;If L is NULL return empty list
+             ((LIST? (CAR L)) (append (reverse-general (CDR L))  
+                                  (LIST (reverse-general (CAR L)))))
+             (ELSE (append (reverse-general (CDR L))    
+                       (LIST (CAR L)))))
                                  
+)
+
+;;; Function returns sum of numbers in list without addressing
+;;; nested lists
+(DEFINE (sum-up-numbers-simple L)
+        (COND
+             ((NULL? L) 0)
+             ((NUMBER? (CAR L)) (+ (CAR L) (sum-up-numbers-simple (CDR L))))
+             (ELSE (sum-up-numbers-simple (CDR L)))
+         )
 )

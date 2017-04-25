14289e31705e8b93206e48337345f7607d0efed7
tems4real <oreoluwa.onatemowo@gmail.com>
2017-04-25 16:44:55 -0400

    Now performs a deep reverse of a list L

--- fp.rkt
+++ fp.rkt
@@ -1,8 +1,11 @@
-;;;Currently performs a shallow reverse of L
+;;; Function reverse-general takes a list L
+;;; and performs a deep reverse of L
 (DEFINE (reverse-general L)
         (COND
-        ((NULL? L) '())
-        (ELSE (append (reverse-general (CDR L))
+        ((NULL? L) '()) ;;;If L is NULL return empty list
+        ((LIST? (CAR L)) (append (reverse-general (CDR L))  
+                           (LIST (reverse-general (CAR L)))))
+        (ELSE (append (reverse-general (CDR L))    
                                  (LIST (CAR L)))))
                                  
 )

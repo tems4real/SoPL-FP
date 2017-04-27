61ed770ceb38b2087762b24eaca9f36a76170361
tems4real <tems4real@gmail.com>
2017-04-27 13:24:20 -0400

    Added selection sort and find first greater functions

--- fp.rkt
+++ fp.rkt
@@ -1,5 +1,5 @@
-;;; Function reverse-general takes a list L
-;;; and performs a deep reverse of L
+; Function reverse-general takes a list L
+; and performs a deep reverse of L
 (DEFINE (reverse-general L)
         (COND
              ((NULL? L) '()) ;;;If L is NULL return empty list
@@ -10,8 +10,8 @@
                                  
 )
 
-;;; Function returns sum of numbers in list without addressing
-;;; nested lists
+; Function returns sum of numbers in list without addressing
+; nested lists
 (DEFINE (sum-up-numbers-simple L)
         (COND
              ((NULL? L) 0)
@@ -20,8 +20,8 @@
          )
 )
 
-;;; Function returns sum of numbers in input list including
-;;; nested lists
+; Function returns sum of numbers in input list including
+; nested lists
 (DEFINE (sum-up-numbers-general L)
         (COND
              ((NULL? L) 0)
@@ -32,8 +32,8 @@
          )
 )
 
-;;; Function that given 2 simple lists, L1, L2 returns the minimum
-;;; of the numbers
+; Function that given 2 simple lists, L1, L2 returns the minimum
+; of the numbers
 
 (DEFINE (return-num-list L)
         (COND
@@ -42,3 +42,38 @@
              (ELSE (APPEND (CAR L) (return-num-list (CDR L))))
         )
 )
+
+; selection sort
+
+(DEFINE (selection L) 
+   (cond ( (null? L) '() )
+         ( else (cons (smallest L (car L)) 
+                      (selection (remove L (smallest L (car L)))))
+                                               
+         )
+   )
+)
+
+(define (remove L A)                
+  (cond ( (null? L) '() )           
+        ( (= (car L) A) (cdr L))
+        (else (cons (car L)(remove (cdr L) A)))
+  )
+)
+
+(define (smallest L A)
+  (cond ( (null? L) A)
+        ( (< (car L) A) (smallest (cdr L)(car L)))
+        (else (smallest (cdr L) A ))
+  )
+)
+
+(DEFINE (first-larger L N)
+        (COND
+             ((NULL? L) #F)
+             ((>= (CAR L) N) (first-larger (CDR L) N))
+             (ELSE (CAR L))
+         )
+)
+
+; 
